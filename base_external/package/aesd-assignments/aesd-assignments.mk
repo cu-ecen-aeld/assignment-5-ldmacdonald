a3a60d31624532c0dec5ae62332cd819e93e03ff
Dale MacDonald <lawrence.macdonald@colorado.edu>
2024-10-11 20:58:48 -0700

    Updating commit hash

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 'f4d9d2e1a63bb4174540c96cd261c38876791866'
+AESD_ASSIGNMENTS_VERSION = '67ca310d28401837757eae050c8829860a6928c8'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
